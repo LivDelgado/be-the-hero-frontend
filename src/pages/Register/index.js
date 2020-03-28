e1363336f9fad93104cc3f9d4ec950c2b648feb4
Lívia Delgado <livia.delgado.ac@gmail.com>
2020-03-27 21:48:41 -0300

    Correções na página de cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -15,9 +15,9 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
                     
-                    <Link to="/register" className="back-link">
+                    <Link to="/" className="back-link">
                         <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho cadastro
+                        Voltar
                     </Link>
                 </section>
 
